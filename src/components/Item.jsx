de757703678428c7cc4df83f6a1a813a0fcbec3b
NikaBotchorishvili <nikabotchorishvili@gmail.com>
2022-11-29 19:02:38 +0400

    Total price in the cart

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,15 +1,21 @@
 import React, { useContext } from "react";
 
 import { Context } from "../context/Context";
+
 function Cart({ item }) {
-	const { removeCartItem } = useContext(Context)
+	const { removeCartItem } = useContext(Context);
+
+
 	return (
 		<div className="cart-item">
 			<div className="item-container">
-				<i onClick={() => removeCartItem(item.id)} className="ri-delete-bin-line"></i>
+				<i
+					onClick={() => removeCartItem(item.id)}
+					className="ri-delete-bin-line"
+				></i>
 				<img src={item.url} width="130px" />
 			</div>
-			<p className="price">$5.99</p>
+			<p className="price"></p>
 		</div>
 	);
 }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -11,11 +11,16 @@ function Cart() {
 	const cartItemElements = cartItems.map((item) => {
 		return <Item key={item.id} item={item} />;
 	});
+
+	const totalPrice = (cartItems.length * 5.99).toLocaleString("GA", {
+		style: "currency",
+		currency: "Gel",
+	});
 	return cartItemElements.length > 0 ? (
 		<main className="cart-page">
 			<h1>Check out</h1>
 			<div className="items-container">{cartItemElements}</div>
-			<p className="total-cost">Total: </p>
+			<p className="total-cost">Total: { TotalPrice } </p>
 			<div className="order-button">
 				<button>Place Order</button>
 			</div>
